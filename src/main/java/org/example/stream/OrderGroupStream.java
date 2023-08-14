5f524a750ce3a086f950e785d1749d0403b38d7d
sambhav.jain <sambhav.jain@hub.com>
2023-08-14 22:14:23 +0530

    Init

--- src/main/java/org/example/stream/OrderGroupStream.java
+++ src/main/java/org/example/stream/OrderGroupStream.java
@@ -2,11 +2,14 @@ package org.example.stream;
 
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.example.model.Order;
 
 public class OrderGroupStream {
 
     public Topology createTopology() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<Integer, Order> orderStream = StreamUtils.getOrderStream(streamsBuilder);
         return streamsBuilder.build();
     }
 }
