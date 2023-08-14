ea915d0a242dd9dc9911972b536bdf3763264c55
sambhav.jain <sambhav.jain@hub.com>
2023-08-14 22:54:22 +0530

    Init

--- src/main/java/org/example/stream/OrderGroupStream.java
+++ src/main/java/org/example/stream/OrderGroupStream.java
@@ -1,15 +1,27 @@
 package org.example.stream;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
+import org.example.config.CustomSerdes;
 import org.example.model.Order;
 
+@Slf4j
 public class OrderGroupStream {
 
     public Topology createTopology() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         KStream<Integer, Order> orderStream = StreamUtils.getOrderStream(streamsBuilder);
-        return streamsBuilder.build();
+        KGroupedStream<String, Order> orderGroupedByItem = orderStream
+                .groupBy((k, v) -> v.getItemDescription(), Grouped.with("groupoo", Serdes.String(), CustomSerdes.Order()));
+
+
+        KTable<String, Long> stringLongKTable = orderGroupedByItem.count();
+        stringLongKTable.toStream().print(Printed.toSysOut());
+        final Topology topology = streamsBuilder.build();
+        log.info(topology.describe().toString());
+        return topology;
     }
 }
--- src/test/java/org/example/stream/OrderGroupStreamTest.java
+++ src/test/java/org/example/stream/OrderGroupStreamTest.java
@@ -6,7 +6,6 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.example.config.CustomSerdes;
 import org.example.model.Order;
-import org.example.model.Payment;
 import org.instancio.InstancioApi;
 import org.junit.jupiter.api.Test;
 
@@ -14,32 +13,22 @@ import java.time.Instant;
 import java.util.Properties;
 import java.util.stream.Stream;
 
-class OrderPaymentStreamJoinTest {
+import static org.example.stream.OrderPaymentStreamJoinTest.ORDER_COUNT;
 
-    public static final int ORDER_COUNT = 100;
-
+class OrderGroupStreamTest {
 
     @Test
-    void testJoin() {
+    void createTopology() {
         Instant reference = Instant.now().minusSeconds(9999999);
         var timestampSupplier = new TimestampSupplier(reference, 0);
         InstancioApi<Order> orderInstancioApi = MockDataUtil.getGenerate(timestampSupplier);
-
-        TimestampSupplier paymentTimestampSupplier = new TimestampSupplier(reference, 5);
-        InstancioApi<Payment> paymentInstancioApi = MockDataUtil.getPaymentSupplier(paymentTimestampSupplier);
-
-        OrderPaymentStreamJoin stream = new OrderPaymentStreamJoin();
-        Topology topology = stream.createStream();
         Properties config = StreamUtils.getProperties();
+        Topology topology = new OrderGroupStream().createTopology();
         try (TopologyTestDriver testDriver = new TopologyTestDriver(topology, config)) {
-            TestInputTopic<Integer, Order> orderTopic = testDriver.createInputTopic("orders", Serdes.Integer().serializer(), CustomSerdes.Order().serializer());
+            TestInputTopic<Integer, Order> orderTopic = testDriver
+                    .createInputTopic("orders", Serdes.Integer().serializer(), CustomSerdes.Order().serializer());
             Stream<Order> order = orderInstancioApi.stream().limit(ORDER_COUNT);
             order.toList().forEach(o -> orderTopic.pipeInput(o.getId(), o));
-            //payments
-            TestInputTopic<Integer, Payment> paymentTopic = testDriver.createInputTopic("payments", Serdes.Integer().serializer(), CustomSerdes.Payment().serializer());
-            paymentInstancioApi.stream().limit(ORDER_COUNT).toList().forEach(p -> paymentTopic.pipeInput(p.getOrderId(), p));
         }
-
     }
-
 }
