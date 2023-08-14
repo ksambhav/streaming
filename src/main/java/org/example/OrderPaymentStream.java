73fd1a098c1cd7f58e5b36e1d221784a0b17298e
sambhav.jain <sambhav.jain@hub.com>
2023-08-14 18:49:51 +0530

    Init

--- src/main/java/org/example/OrderPaymentStream.java
+++ src/main/java/org/example/OrderPaymentStream.java
@@ -1,14 +1,13 @@
 package org.example;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.errors.StreamsException;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.processor.TimestampExtractor;
 
 @Slf4j
 public class OrderPaymentStream {
@@ -16,11 +15,13 @@ public class OrderPaymentStream {
     public Topology createStream() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         KStream<Integer, Order> orderStream = streamsBuilder.stream("orders",
-                Consumed.with(Serdes.Integer(), CustomSerdes.Order()).withTimestampExtractor(new TimestampExtractor() {
-                    @Override
-                    public long extract(ConsumerRecord<Object, Object> consumerRecord, long l) {
-                        return 0;
+                Consumed.with(Serdes.Integer(), CustomSerdes.Order()).withTimestampExtractor((consumerRecord, l) -> {
+                    Order value = (Order) consumerRecord.value();
+                    long epochSecond = value.getCreatedOn().getEpochSecond();
+                    if (epochSecond < l) {
+                        throw new StreamsException("Not the right timing " + epochSecond);
                     }
+                    return epochSecond;
                 }));
         orderStream
                 .filter((k, o) -> o.getAmount().intValue() > 0)
--- src/test/java/org/example/OrderPaymentStreamTest.java
+++ src/test/java/org/example/OrderPaymentStreamTest.java
@@ -9,23 +9,51 @@ import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.instancio.Instancio;
+import org.instancio.InstancioApi;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
 import java.util.Properties;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static org.apache.kafka.common.serialization.Serdes.String;
+import static org.instancio.Select.field;
 
 class OrderPaymentStreamTest {
 
     public static final ObjectMapper MAPPER = new ObjectMapper();
+    public static final int ORDER_COUNT = 1000;
+
 
     static {
         MAPPER.registerModule(new JavaTimeModule());
     }
 
+    public static class TimestampSupplier implements Supplier<Instant> {
+
+        public static final Instant referenceTimeStamp;
+        private int offset = 0;
+
+        static {
+            referenceTimeStamp = Instant.now().minusSeconds(99999999L);
+        }
+
+        @Override
+        public Instant get() {
+            return referenceTimeStamp.plusSeconds(offset++);
+        }
+    }
+
+
     @Test
     void createStream() throws JsonProcessingException {
+
+        final Instant[] reference = {Instant.now().minusSeconds(9999999L)};
+        InstancioApi<Order> orderInstancioApi = Instancio.of(Order.class)
+                .generate(field(Order::getDescription), gen -> gen.text().loremIpsum().words(10))
+                .supply(field(Order::getCreatedOn), TimestampSupplier::new);
+
         OrderPaymentStream stream = new OrderPaymentStream();
         Topology topology = stream.createStream();
         Properties config = new Properties();
@@ -34,7 +62,7 @@ class OrderPaymentStreamTest {
         config.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, String().getClass().getName());
         try (TopologyTestDriver testDriver = new TopologyTestDriver(topology, config)) {
             TestInputTopic<Integer, Order> orderTopic = testDriver.createInputTopic("orders", Serdes.Integer().serializer(), CustomSerdes.Order().serializer());
-            Stream<Order> order = Instancio.stream(Order.class).limit(10);
+            Stream<Order> order = orderInstancioApi.stream().limit(ORDER_COUNT);
             order.toList().forEach(o -> orderTopic.pipeInput(o.getId(), o));
         }
 
