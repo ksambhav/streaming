ec9103ded3f9cfd035c5185e98d0484bde50499f
sambhav.jain <sambhav.jain@hub.com>
2023-10-26 16:16:55 +0530

    Init

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+rm -rf application
+rm -rf dependencies
+rm -rf snapshot-dependencies
+rm -rf spring-boot-loader
 ./mvnw package -DskipTests
 java -Djarmode=layertools -jar target/my-application.jar extract
-#docker build -t samsoft
+docker build -t samsoft .
