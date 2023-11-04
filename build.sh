a96677d22477b1e9fababc3d2b5062a043448d97
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2023-11-04 09:51:56 +0530

    Fixed build script error in windows

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./mvnw package -DskipTests
-cp start.sh target/extracted
+cp start.sh target/extracted/start.sh
 java -Djarmode=layertools -jar target/my-application.jar extract --destination target/extracted
 docker build -f Dockerfile -t samsoft target/extracted
